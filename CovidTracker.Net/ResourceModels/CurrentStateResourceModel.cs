05093bb66c6296157b7dfde296a05f1cf8fdbc22
Derek Stegelman <dstegelman@users.noreply.github.com>
2020-11-14 17:58:54 -0700

    Adding more fields

--- CovidTracker.Net/ResourceModels/CurrentStateResourceModel.cs
+++ CovidTracker.Net/ResourceModels/CurrentStateResourceModel.cs
@@ -6,6 +6,13 @@ namespace CovidTracker.Net.ResourceModels
     {
         public string state { get; set; }
         public int positive { get; set; }
+        public int probableCases { get; set; }
+        public int negative { get; set; }
+        public int pending { get; set; }
+        public string totalTestResultsSource { get; set; }
+        public int totalTestResults { get; set; }
+        public int hopsitalizedCurrently { get; set; }
+        public int hospitalizedCumulative { get; set; }
         public int positiveIncrease { get; set; }
 
     }
